09ca9bc54365008098cac3c03eeca484c919d3a1
alopez-g <alopez-g@c2r8s3.42madrid.com>
2023-01-18 14:19:11 +0100

    02

--- module01/ex02/src/main.cpp
+++ module01/ex02/src/main.cpp
@@ -1,6 +1,19 @@
-int	main(int argc, char **argv)
+#include <string>
+#include <iostream>
+
+int	main(void)
 {
-	(void)argc;(void)argv;
+	std::string	salute = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &salute;
+	std::string	&stringREF = salute;
+
+	std::cout << &salute << "\n";
+	std::cout << stringPTR << "\n";
+	std::cout << &stringREF << "\n";
+
+	std::cout << salute << "\n";
+	std::cout << *stringPTR << "\n";
+	std::cout << stringREF << "\n";
 
 	return (0);
 }
