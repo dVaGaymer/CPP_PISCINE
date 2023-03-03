989ffbce34f413d7cd06d71dd21554cd73737d9d
alopez-g <alopez-g@c2r8s3.42madrid.com>
2023-03-03 22:49:28 +0100

    main_test

--- module01/ex06/src/main.cpp
+++ module01/ex06/src/main.cpp
@@ -1,9 +1,12 @@
 #include "Harl.hpp"
 
+void	l(void){system("leaks harl");}
+
 int	main(int argc, char **argv)
 {
 	Harl	h;
 
+	atexit(l);
 	if (argc != 2)
 	{
 		std::cout << "Harl is making uninteligible sounds\n";
